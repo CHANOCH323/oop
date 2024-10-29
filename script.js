47f2a8732ca50f229d5d3eaba89df65ebabfebde
CHANOCH323 <Ch0533119759@gmail.com>
2024-10-29 13:33:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function findMax() {
 function findMax2(...numbers) {
     return { max: Math.max(...numbers), min: Math.min(...numbers) }
 }
+
+const pet = {
+    name: 'Buddy',
+    age: 3, 
+    type: 'Dog',
+}
+const petActions = {
+    printPet: function(obj) {
+        console.log(obj);
+    }
+
+}
+petActions.printPet(pet)
