45d971498be8a22071d059094e2467c990f59a93
CHANOCH323 <Ch0533119759@gmail.com>
2024-10-29 13:13:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function findMax(){
-    return({max:Math.max(...arguments),min:Math.min(...arguments)})
+function findMax() {
+    return ({ max: Math.max(...arguments), min: Math.min(...arguments) })
+}
+
+function findMax2(...numbers) {
+    return { max: Math.max(...numbers), min: Math.min(...numbers) }
 }
